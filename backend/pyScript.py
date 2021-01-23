6f3969114061c17b50a71e694c8d8fb5eb0030ca
Krunal Patel <krunalp2102@gmail.com>
2021-01-23 00:57:29 -0500

    script changes

--- backend/pyScript.py
+++ backend/pyScript.py
@@ -4,7 +4,8 @@ zip_dir = "./gltf";
 for subdir, dirs, files in os.walk(zip_dir):
     for zipp in files:
         old_file_name = zipp;
-        file_name = (zipp.split('.')[0]).split(" ").join("_");
+        file_name = (zipp.split('.')[0]).split(" ");
+        file_name = "_".join(file_name);
         filepath= os.path.join(subdir, zipp);
         zip_ref = zipfile.ZipFile(filepath, 'r')
         os.mkdir(zip_dir+"/"+file_name);
